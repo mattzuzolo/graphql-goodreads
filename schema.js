99a5999472e70fdf77b4496d262727126645d965
mattzuzolo <mattzuzolo@outlook.com>
2018-11-03 18:01:45 -0400

    Now fetching only when ID is presented, rather than always showing everything

--- schema.js
+++ schema.js
@@ -1,6 +1,6 @@
 const fetch = require("node-fetch");
 const util = require("util");
-const parseXML = util.promisify(require("xml2js").parseString);
+const parseXML = util.promisify(require('xml2js').parseString);
 
 const {
   GraphQLSchema,
@@ -19,9 +19,10 @@ const BookType = new GraphQLObjectType({
   fields: () => ({
     title: {
       type: GraphQLString,
-      resolve: xml => 
-        xml.title[0]
-    
+      //book data being fetched in AuthorType by id
+      resolve: xml => xml.GoodreadsResponse.book[0].title[0]
+        // xml.title[0]
+
     },
   })
 })
@@ -33,13 +34,31 @@ const AuthorType = new GraphQLObjectType({
   fields: () => ({
     name: {
       type: GraphQLString,
-      resolve: xml => 
+      resolve: xml =>
         xml.GoodreadsResponse.author[0].name[0]
     },
     books: {
       type: new GraphQLList(BookType),
-      resolve: xml => 
-        xml.GoodreadsResponse.author[0].books[0].book
+      resolve: xml => {
+        //xml.GoodreadsResponse.author[0].books[0].book
+
+        //Save id from fetch -- underscore is result of dealing with XML to JS weirdness
+        const ids = xml.GoodreadsResponse.author[0].books[0].book.map(elem => elem.id[0]._)
+        //fetch for each id from books end point
+        console.log("IDS:", ids)
+        console.log("currently fetching books...")
+        return Promise.all(ids.map(id => fetch(`https://www.goodreads.com/book/show/${id}.xml?key=PlzRig5nleiS3XvYNYYPfA`)
+        .then(response => response.text())
+        .then(parseXML)
+      ))
+      //result passed into each BookType
+
+
+      }
+
+
+
+
     }
   })
 })
